dbfba40333b34acca161a8b8d63533812e8c2f35
JAVIERCLO <cha23132@uvg.edu.gt>
2025-08-12 18:35:23 -0600

    Pequeños cambios en la estructura y layout terminado

--- Lab4/app/src/main/java/com/jcl/lab4/MainActivity.kt
+++ Lab4/app/src/main/java/com/jcl/lab4/MainActivity.kt
@@ -5,21 +5,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jcl.lab4.ui.theme.Lab4Theme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,67 +47,106 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Pantalla1(modifier: Modifier = Modifier) {
 
-    //Imagen de fondo
+    //Contenedor de pantalla1
     Box(
         modifier = Modifier
-            .fillMaxSize()
+        .fillMaxSize()
+            .border(
+                width = 10.dp,
+                color = Color(0xFF078B45), //Verde utilizado en la página de UVG
+                shape = RectangleShape
+            )
     ){
+        //Imagen de fondo
         Image(
             painter = painterResource(id = R.drawable.logouvg),
             contentDescription = "Logo de UVG de fondo",
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize()
+            .alpha(0.3f),
             contentScale = ContentScale.Fit
-            //Investigar cómo difuminar
         )
     }
     //Columna principal
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
     ){
      //Texto de título UVG
         Text(
-            text = "Universidad del Valle de Guatemala"
-            //style = (investigar el tipo de fuente y tamaño)
+            text = "Universidad del Valle de Guatemala",
+            style = TextStyle(
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+
         )
+
+        Spacer(modifier = Modifier.height(50.dp))
         //Texto de plataformas móviles
         Text(
-            text = "Programación de plataformas móviles, sección 30"
-            //style = (investigar tipo de fuente y tamaño)
+            text = "Programación de plataformas móviles, sección 30",
+            style = TextStyle(
+                fontSize = 22.sp,
+                textAlign = TextAlign.Center
+            )
         )
-        //Caja para integrantes
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
 
+        Spacer(modifier = Modifier.height(50.dp))
+        //Fila para integrantes
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
         )
         {
             Text(
-                text = "Integrantes"
+                text = "Integrantes",
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
 
             )
             Text(
-                text = "Javier Chávez\nCristiano Ronaldo\nLionel Messi"
+                text = "Javier Chávez\nEnya Gálvez\nAxel Cruz",
+                style = TextStyle(
+                    textAlign = TextAlign.Center
+                )
 
             )
         }
+
+        Spacer(modifier = Modifier.height(50.dp))
+
         //Fila para catedrático
         Row(
-            modifier = Modifier
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
         )
         {
             Text(
-                text = "Catedrático"
-                //style =
+                text = "Catedrático",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                )
             )
             Text(
                 text = "Juan Carlos Durini"
-                //style =
             )
         }
+
+        Spacer(modifier = Modifier.height(50.dp))
+
         Text(
-            text = "Javier Chávez\n23132"
+            text = "Javier Chávez\n23132",
+            style = TextStyle(
+                textAlign = TextAlign.Center
+            )
         )
     }
 
