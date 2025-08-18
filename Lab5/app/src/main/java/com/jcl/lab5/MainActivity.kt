a23e152f3ba2d2f0010fdaea65d27b5e45c95371
JAVIERCLO <cha23132@uvg.edu.gt>
2025-08-17 21:30:44 -0600

    Laboratorio 5 terminado

--- Lab5/app/src/main/java/com/jcl/lab5/MainActivity.kt
+++ Lab5/app/src/main/java/com/jcl/lab5/MainActivity.kt
@@ -1,29 +1,47 @@
 package com.jcl.lab5
 
+import android.R.attr.button
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Refresh
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jcl.lab5.ui.theme.Lab5Theme
+import android.net.Uri
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.material.icons.outlined.Place
+import androidx.compose.material3.ElevatedCard
 
 
 class MainActivity : ComponentActivity() {
@@ -43,48 +61,73 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun Pantalla1(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
     //Columna principal de la pantalla
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .windowInsetsPadding(WindowInsets.safeDrawing),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Top
     )
     {
         //Fila celeste
         Row(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .background(Color.Cyan),
             horizontalArrangement = Arrangement.SpaceAround
-            //Color celeste
         )
         {
             //Icono azul
+            Icon(Icons.Outlined.Refresh, contentDescription = null, tint = Color.Blue)
             //texto de actualización disponible
+            Text(text = "Actualización disponible")
             //Texto de Descargar con link a playStore
+            TextButton(onClick = {
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=com.whatsapp")
+                )
+                context.startActivity(intent)
+            }) { Text("Descargar") }
         }
 
         //Fila con fecha de cumpleaños y botón de terminar jornada
         Row(
             modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround
+                .fillMaxWidth()
+                .padding(22.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
         )
         {
-            //Texto de viernes
-            //Texto de fecha de cumpleaños
+            Column(
+                modifier = Modifier,
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Domingo", fontWeight = FontWeight.Bold, fontSize = 32.sp)
+                //Texto de fecha de cumpleaños
+                Text(text = "7 de Septiembre")
+            }
+
             //Botón de terminar jornada
+            OutlinedButton(onClick = {  }) {
+                Text("Terminar jornada", color = Color.Magenta)
+            }
         }
 
-        //Caja para información del restaurante
-        Card(
+        //Card para información del restaurante
+        ElevatedCard(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
+                .padding(16.dp)
         )
         {
             Column(
                 modifier = Modifier
+                .padding(16.dp)
             )
             {
                 // Columna para los textos del restaurante
@@ -92,9 +135,32 @@ fun Pantalla1(modifier: Modifier = Modifier) {
                     modifier = Modifier
                         .fillMaxWidth()
                 ) {
-                    // Nombre
-                    // Dirección
-                    // Horarios
+                    //fila para nombre y icono de ubicación
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    )
+                    {
+                        //Nombre de restaurante
+                        Text(
+                            text = "Saúl",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 28.sp
+                        )
+                        Spacer(Modifier.weight(1f))
+                        IconButton(onClick = {
+                            val uri = Uri.parse("geo:14.5965,-90.5133?q=14.5965,-90.5133(Saúl)")
+                            val intent = Intent(Intent.ACTION_VIEW, uri).apply {
+                                setPackage("com.google.android.apps.maps")
+                            }
+                            context.startActivity(intent)
+                        }) {
+                            Icon(Icons.Outlined.Place, contentDescription = "Direcciones")
+
+                        }
+                    }
+                    Text(text = "6a. Avenida 15-64, Zona 10")
+                    Text(text = "7:00-22:00")
                 }
 
                 // Fila inferior para los botones
@@ -103,8 +169,19 @@ fun Pantalla1(modifier: Modifier = Modifier) {
                         .fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    // Botón Iniciar
+                    Button(
+                        onClick = {
+                            Toast.makeText(context, "Javier Alejandro Chávez León", Toast.LENGTH_SHORT).show()
+                        }
+                    ) {
+                        Text("Iniciar")
+                    }
                     // Botón Detalles
+                    TextButton(onClick = {
+                        Toast.makeText(context, "Cocina internacional moderna \nQQ", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Text("Detalles")
+                    }
                 }
             }
         }
